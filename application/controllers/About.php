c1e5b4711366c10e8e10a1848f21da1281994bb6
a00770012 <bcit@bcit-PC>
2015-01-17 17:35:56 -0800

    COMP4711 tutorial 2c
    Applied base controller. 
    @TODO: fix php error: Call to a member function parse() on null 
    Signed-off-by:a00770012 <bcit@bcit-PC>

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,15 +8,17 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class About extends CI_Controller {
+class About extends Application {
 
-	/**
-	 * Gallery Page for this controller.
-	 */
-	public function index()
-	{
-		$this->load->view('about');
-	}
+    /**
+     * Gallery Page for this controller.
+     */
+    public function index() {
+//        $this->load->view('about');
+        $this->data['pagebody'] = 'about';
+        $this->render();
+    }
+
 }
 
 /* End of file About.php */
--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,15 +8,17 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Gallery extends CI_Controller {
+class Gallery extends Application {
 
-	/**
-	 * Gallery Page for this controller.
-	 */
-	public function index()
-	{
-		$this->load->view('gallery');
-	}
+    /**
+     * Gallery Page for this controller.
+     */
+    public function index() {
+//        $this->load->view('gallery');
+        $this->data['pagebody'] = 'gallery';
+        $this->render();
+    }
+
 }
 
 /* End of file Gallery.php */
