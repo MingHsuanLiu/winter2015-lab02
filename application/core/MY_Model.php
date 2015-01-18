9802c7e7504445fa69d1fc111c31cf469a22d534
a00770012 <bcit@bcit-PC>
2015-01-18 12:49:16 -0800

    COMP4711 tutorial 2c
    Added function newest() in MY_Model.php, to just display latest 3 images.
    
    @TODO: fix php database error: Unable to connect to your database server using the provided settings.
    Signed-off-by:a00770012 <bcit@bcit-PC>

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -11,4 +11,19 @@ class Images extends CI_Model {
     function __construct() {
         parent::__construct();
     }
+    
+    // return all images, descending order by date
+    function all() {
+        $this->db->order_by("id", "desc");
+        $query = $this->db->get('images');
+        return $query->result_array();
+    }
+    
+    // return just the 3 newest images.
+    function newest() {
+        $this->db->order_by("id", "desc");
+        $this->db->limit(3);
+        $query = $this->db->get('images');
+        return $query->result_array();
+    }
 }
