4eea4674eee48e9d3cb943026de6280380b81dec
a00770012 <bcit@bcit-PC>
2015-01-18 13:34:45 -0800

    COMP4711 tutorial 2c
    Added function newest() in MY_Model.php, to just display latest 3 images.
    
    @FIXED: fix the typo in database.php
    @TODO: fix "A Database Error Occurred: Unable to select the specified database: picassos"
    Signed-off-by:a00770012 <bcit@bcit-PC>

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -14,7 +14,26 @@ class Gallery extends Application {
      * Gallery Page for this controller.
      */
     public function index() {
-//        $this->load->view('gallery');
+        // get all the images from our model
+        $pix = $this->imges->all();
+        
+        // build an array of formatted cells from them
+        foreach ($pix as $picture)
+            $cell[] = $this->parser->parse('_cell', (array)$picture, true);
+        
+        // prime the table class
+        $this->load->library('table');
+        $parms = array(
+            'table_open' => '<table class="gallery">',
+            'cell_start' => '<td class="oneimage">',
+            'cell_alt_start' => '<td class="oneimage">'
+        );
+        $this->table->set_template($parms);
+        
+        // finally! generate the table
+        $rows = $this->table->make_columns($cells, 3);
+        $this->data['thetable'] = $this->generate($rows);
+
         $this->data['pagebody'] = 'gallery';
         $this->render();
     }
--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -10,9 +10,7 @@ class Application extends CI_Controller {
     protected $data = array();      // parameters for view components
     protected $id;    // identifier for our content
     protected $choices = array(// our menu navbar
-        'Home' => '/',
-        'Gallery' => '/gallery',
-        'About' => 'about'
+	'Home' => '/', 'Gallery' => '/gallery', 'About' => '/about'
     );
 
     /**
@@ -20,19 +18,19 @@ class Application extends CI_Controller {
      * Establish view parameters & load common helpers
      */
     function __construct() {
-        parent::__construct();
-        $this->data = array();
-        $this->data['pagetitle'] = 'Sample Image Gallery';
+	parent::__construct();
+	$this->data = array();
+	$this->data['pagetitle'] = 'Simple Image Gallery';
     }
 
     /**
      * Render this page
      */
     function render() {
-        $this->data['menubar'] = build_menu_bar($this->choices);
-        $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
-        $this->data['data'] = &$this->data;
-        $this->parser->parse('template', $this->data);
+	$this->data['menubar'] = build_menu_bar($this->choices);
+	$this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
+	$this->data['data'] = &$this->data;
+	$this->parser->parse('_template', $this->data);
     }
 
 }
